f828d29381fa87892a7dffcdbcc588bdd80ecafc
Steve Renner <che1111@hotmail.com>
2020-03-13 09:14:28 -0700

    added ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -12,7 +12,18 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-
+    # for index, find length and insert into hashtable
+    for idx in range(0, length):
+        hash_table_insert(ht, weights[idx], idx)
+    # second loop not nested avoids quadratic runtime
+    for idx in range(0, length):
+        answer = hash_table_retrieve(ht, limit - weights[idx])
+    # if hash table contains an entry for limit - weight:
+        if answer:
+    # taking data from hashtable and returning
+    # weights sum to limit
+            return max(idx, answer), min(idx, answer)
+    # if key doesn't exist, or if indices don't fit in correct index
     return None
 
 
